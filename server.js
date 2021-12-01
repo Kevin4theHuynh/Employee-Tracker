5c5cc8a94c14959e684a70795565b52206607224
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-01 11:40:08 -0800

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const mysql = require('mysql2')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    },
+    console.log('Connected')
+)
+
+
 
 
 
