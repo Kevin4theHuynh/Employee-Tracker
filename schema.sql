e686a7c02b9e579b9545b97f950a0d6bfbf5b66f
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-01 11:20:31 -0800

    added seeds

--- schema.sql
+++ schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE employee_db
 
 USE employee_db;
 
-CREATE Table department (
+CREATE TABLE department (
     id: INT PRIMARY KEY,
     department_name: VARCHAR(30)
 );
 
-CREATE Table role (
+CREATE TABLE role (
     id: INT PRIMARY KEY,
     title: VARCHAR(30)
     salary: DECIMAL
-    departmnet_id: INT
+    department_id: INT
 );
 
-CREATE Table employee (
+CREATE TABLE employee (
     id: INT PRIMARY KEY
     first_name: VARCHAR(30)
     last_name: VARCHAR(30)
---
+++ seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (deparment_name) VALUES
+("Cook"),
+("Engineer"),
+("Dishwasher"),
+("Manager"):
+
+INSERT INTO roles (title, salary, department_id) VALUES
+("Lead Cook", 10000, 1),
+("Lead Engineer", 100, 2),
+("Dishwasher", 100000, 3),
+("Restaurant Manager", 10, 4);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES
+("Cody", "Nguyen", 1),
+("Jericho", "Pham", 2),
+("Johnathan", "Ho", 3),
+("Kevin", "Huynh", 4)
