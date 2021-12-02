880c866faea929d9d010cd6b3e45a7f7f27dfffa
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-01 21:18:05 -0800

    database

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const mysql = require('mysql2')
-const db = require("./db")
+const dotenv = require('dotenv')
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
-
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+  },
+  console.log(`Connected`)
+)
 
 app.use((req, res) => {
     res.status(404).end();
