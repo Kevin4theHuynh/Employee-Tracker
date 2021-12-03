fee90124052c36c87b6a7cd5deae8c0baa2d4a87
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-02 21:56:19 -0800

    added findDepartment

--- db/index.js
+++ db/index.js
@@ -6,9 +6,10 @@ findEmployee()
 {return this.index.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id`)}
 
 findRole()
-{return this.index.promise().query(`SELECT role.id, role.title, role,salary, role.department.id`)}
+{return this.index.promise().query(`SELECT role.id, role.title, role.salary, role.department_id`)}
 
-find
+findDepartment()
+{return this.index.promise().query(`SELECT department.id, department.department_name`)}
 
 db.query('SELECT * FROM department', function (err, results) {
     console.log(results)
